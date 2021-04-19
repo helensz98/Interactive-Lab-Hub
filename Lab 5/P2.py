89079938968ec4eebd64c52bba0398470a7b7ff1
helensz98 <41175006+helensz98@users.noreply.github.com>
2021-04-19 17:11:37 -0400

    Part 2 code

---
+++ Lab 5/P2.py
@@ -0,0 +1,109 @@
+#nano indentation is super weird. 
+# for loops and if have different requirements
+# this version of smileP1.py has indentation specifically adjusted for nano file in my computer
+
+import numpy as np
+import cv2
+import sys
+
+from gtts import gTTS
+import os
+
+import qwiic_button 
+
+button = qwiic_button.QwiicButton(0x6F)
+button.begin()
+
+t2 = 'Give me a smile!'
+t3 = 'Three, two, one, cheese!'
+language = 'en'
+
+m2 = gTTS(text=t2, lang=language, slow=False)
+m2.save("t2.mp3")
+m3 = gTTS(text=t3, lang=language, slow=False)
+m3.save("t3.mp3")
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+smile_casecade = cv2.CascadeClassifier('haarcascade_smile.xml')
+
+img=None
+
+webCam = False
+if(len(sys.argv)>1):
+  try:
+     print("I'll try to read your image");
+     img = cv2.imread(sys.argv[1])
+     if img is None:
+        print("Failed to load image file:", sys.argv[1])
+  except:
+     print("Failed to load the image are you sure that:", sys.argv[1],"is a path to an image?")
+else:
+  try:
+     print("Trying to open the Webcam.")
+     cap = cv2.VideoCapture(0)
+     if cap is None or not cap.isOpened():
+        raise("No camera")
+     webCam = True
+  except:
+     img = cv2.imread("../data/test.jpg")
+     print("Using default image.")
+
+
+used = []
+selfie = False
+
+while(True):
+
+  if webCam:
+     ret, img = cap.read()
+
+  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+  faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+  nFace = len(faces)
+  nSmile = 0
+
+  users = False
+  for (x,y,w,h) in faces:
+      users = True
+      img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+      roi_gray = gray[y:y+h, x:x+w]
+      roi_color = img[y:y+h, x:x+w]
+
+      smiles = smile_casecade.detectMultiScale(roi_gray, 1.8, 20)
+      nSmile = len(smiles)
+
+      for (sx, sy, sw, sh) in smiles:
+          cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh), (0, 0, 255), 2)
+
+  if (button.is_button_pressed()):
+     selfie = True
+     if(nSmile < nFace):
+        os.system("mpg321 out.mp3")
+        os.system("mpg321 t2.mp3")
+        selfie = False
+
+  if (not users):
+     cv2.putText(img, "No user detected", (50, 50), cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+     selfie = False
+
+  elif (selfie):
+     #this is the silence file so that words are not cut off
+     os.system("mpg321 out.mp3")
+     os.system("mpg321 t3.mp3")
+     r, img1 = cap.read()
+     cv2.imwrite('image'+str(len(used))+".png",img1)
+     used.append(1)
+     selfie = False
+
+  if webCam:
+     cv2.imshow('face-detection (press q to quit.)',img)
+     if cv2.waitKey(1) & 0xFF == ord('q'):
+        cap.release()
+        break
+  else:
+     break
+
+cv2.imwrite('faces_detected.jpg',img)
+cv2.destroyAllWindows()
