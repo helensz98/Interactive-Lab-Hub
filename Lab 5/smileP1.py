361dab7aeb2b65a314a41a182c402a52d380f948
helensz98 <41175006+helensz98@users.noreply.github.com>
2021-04-19 11:56:02 -0400

    Update smileP1.py

--- Lab 5/smileP1.py
+++ Lab 5/smileP1.py
@@ -1,6 +1,28 @@
+#nano indentation is super weird. 
+# for loops and if have different requirements
+# this version of smileP1.py has indentation specifically adjusted for nano file in my computer
+
+
 import numpy as np
 import cv2
 import sys
+
+from gtts import gTTS
+import os
+
+#t1 = 'No user detected.'
+#t2 = 'Say cheese!'
+t3 = 'cheese!'
+language = 'en'
+
+#m1 = gTTS(text=t1, lang=language, slow=False)
+#m1.save("no_user.mp3")
+
+#m2 = gTTS(text=t2, lang=language, slow=False)
+#m2.save("cheese.mp3")
+
+m3 = gTTS(text=t3, lang=language, slow=False)
+m3.save("smile.mp3")
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 smile_casecade = cv2.CascadeClassifier('haarcascade_smile.xml')
@@ -10,13 +32,13 @@ img=None
 
 webCam = False
 if(len(sys.argv)>1):
-	try:
+   try:
       print("I'll try to read your image");
       img = cv2.imread(sys.argv[1])
       if img is None:
          print("Failed to load image file:", sys.argv[1])
    except:
-      print("Failed to load the image are you sure that:", sys.argv[1],"is a pas a path to an image?")
+      print("Failed to load the image are you sure that:", sys.argv[1],"is a pa$
 else:
    try:
       print("Trying to open the Webcam.")
@@ -28,7 +50,8 @@ else:
       img = cv2.imread("../data/test.jpg")
       print("Using default image.")
 
-
+state = 0
+pre = 0
 
 while(True):
 
@@ -54,16 +77,31 @@ while(True):
            cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh), (0, 0, 255), 2)
 
    if (not users):
-      cv2.putText(img, "No user detected", (50, 50), cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),5)
+      state = 0
+      pre = 0
+      cv2.putText(img, "No user detected", (50, 50), cv2.FONT_HERSHEY_COMPLEX,1$
+#      if(state != pre):
+#         os.system("mpg321 no_user.mp3")
    elif (not smiling):
-      cv2.putText(img, "Say cheese ------", (50, 50), cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),5)
-
+      state = 1
+      pre = 1
+      cv2.putText(img, "Say cheese ------", (50, 50), cv2.FONT_HERSHEY_COMPLEX,1,(2$
+#      if(state != pre):
+#         os.system("mpg321 cheese.mp3")
+#         pre = state
    else:
-      cv2.putText(img, "Nice smile!", (50, 50), cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),25)
+      state = 2
+      cv2.putText(img, "Nice smile!", (50, 50), cv2.FONT_HERSHEY_COMPLEX,1,(255,255$
+      if(state != pre):
+        #this is the silence file so that words are not cut off
+         os.system("mpg321 out.mp3")
+         os.system("mpg321 smile.mp3")
+         pre = state
 
    if webCam:
       cv2.imshow('face-detection (press q to quit.)',img)
       if cv2.waitKey(1) & 0xFF == ord('q'):
+									    
          cap.release()
          break
    else:
@@ -72,3 +110,10 @@ while(True):
 cv2.imwrite('faces_detected.jpg',img)
 cv2.destroyAllWindows()
 
+
+	    
+	    
+
+
+
+	
